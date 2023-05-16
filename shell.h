c19d6f2e51ede80853e731e188e314e508940f10
syphrose <swabulwenyi@gmail.com>
2023-05-16 02:18:13 -0700

    Removed files

--- shell.h
+++ shell.h
@@ -2,6 +2,14 @@
 #define SHELL_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#define MAX_ARGS 10
 
+void show_prompt(void);
+char** get_input();
+void exec_command(char **args);
 
 #endif
