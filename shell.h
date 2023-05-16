73e7f3a0eb6c8eeb9891b4d884370b047c93ce1f
syphrose <swabulwenyi@gmail.com>
2023-05-16 08:27:21 -0700

    header file

--- shell.h
+++ shell.h
@@ -9,6 +9,7 @@
 #define CMD_LEN 70
 #define PROMPT "$ "
 #define ARGS 10
+extern char **environ;
 
 void exit_line(char *str);
 ssize_t gets_cmd(char *cmd, size_t len);
@@ -17,4 +18,7 @@ size_t _strlen(const char *str);
 size_t _strcspn(const char *str1, const char *str2);
 int _strcmp(const char *string1, const char *string2);
 void exec_token(char *cmd, char *fn);
+char *handle_path(char *cmd);
+char *_strncopy(char *dest, const char *src, int sz);
+
 #endif
