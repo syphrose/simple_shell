c2f8e9a4f0a7bc4650d2a7cf2c2ac823e7b035a2
TechDanny <kokidanny15@gmail.com>
2023-05-19 07:47:31 -0700

    changes on cmd_exist file

--- cmd_exist.c
+++ cmd_exist.c
@@ -12,6 +12,6 @@ int cmd_exists(const char *cmd, const char *dir)
 {
 	char track[CMD_LEN];
 
-	snprintf(track, sizeof(track), "%s/%s", dir, cmd);
+	print_concat(dir, cmd);
 	return (access(track, X_OK) != -1);
 }
