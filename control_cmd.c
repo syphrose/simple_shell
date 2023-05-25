2662e62c751f248dc11b13921a49250a7058ab2e
syphrose <swabulwenyi@gmail.com>
2023-05-24 17:30:26 -0700

    ctrl file

---
+++ control_cmd.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+/**
+ * ctrl - a command to avoid closing the shell
+ *
+ * @sig: makes the shell respond
+ *
+ */
+void ctrl(int sig)
+{
+	(void) sig;
+	write(1, "\n& ", 3);
+}
